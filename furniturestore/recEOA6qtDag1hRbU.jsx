f665ee84be52f62b5b5c81adda3850053382fa61
khanna1223 <79308199+khanna1223@users.noreply.github.com>
2021-06-11 14:54:30 +1000

    Create recEOA6qtDag1hRbU.jsx

---
+++ furniturestore/recEOA6qtDag1hRbU.jsx
@@ -0,0 +1,137 @@
+{
+"id": "recEOA6qtDag1hRbU",
+"stock": 4,
+"price": 99999,
+"colors": [
+"#00ff00",
+"#0000ff"
+],
+"category": "office",
+"images": [
+{
+"id": "attn5F6J7spuRO4hV",
+"url": "https://dl.airtable.com/.attachments/ec4ecc12d40827349c954baa35849b65/57603431/product-5.jpg",
+"filename": "product-5.jpg",
+"size": 189559,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/5286c8182366da3ea11474aaf3e59a6b/8852bb8c",
+"width": 48,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/a2f371071cf292badbb621294758b600/ca963b31",
+"width": 683,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/cb01e54285222bb4869c5f05b9564ca7/ffbaa17b",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attuFjsVeo5qSJZg7",
+"url": "https://dl.airtable.com/.attachments/448713bd2679982f9e1c4a18569a5999/aad57c8b/extra-1.jpeg",
+"filename": "extra-1.jpeg",
+"size": 102108,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/833b36717a11cad62e09bd8b0cc8625a/14f99897",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/08b4931347068eb275232d2fb918485b/6ef72214",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/e97d9afebeed3ceb4b5d5dad1453e7c4/c109b505",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "atttaHO55019VsZgx",
+"url": "https://dl.airtable.com/.attachments/77942cd45ea889e460a60c3288f29eee/76b9bb92/extra-2.jpeg",
+"filename": "extra-2.jpeg",
+"size": 84418,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/74ee7a4d924fdfa58ef6e1ef61be1e87/9d4b12df",
+"width": 50,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/e6ecb2809814ca33e5a26af0becd9305/4d064ed8",
+"width": 717,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/db6ccc6aaebc9a4fae24d8f40a33651e/070fe1ff",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attASynqdHmCsbWYW",
+"url": "https://dl.airtable.com/.attachments/4abd539ddd2c0aba4c8d3a6e777fce5c/8892d90b/extra-3.jpeg",
+"filename": "extra-3.jpeg",
+"size": 107838,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/41dd65da3dafb97021d29a4c2ebe9be5/6cebd581",
+"width": 55,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/155d6bec9045e54246cfff62885aaac9/30b0a67a",
+"width": 788,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/a2db283132a9a3b2a8d1565db53618af/496d761d",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attfwROe3nVm14vHT",
+"url": "https://dl.airtable.com/.attachments/df581a219339aeb777f6f8145a0e8f5a/7f890e00/extra-4.jpeg",
+"filename": "extra-4.jpeg",
+"size": 99481,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/86f08a334c3877aa1ccfc21d14b3227e/507accd0",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/cc2d8c7427a54d5648815f9d3ab114ca/61c21406",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/af48a33eb581c70bbc47e7b9a2be6993/748980d9",
+"width": 3000,
+"height": 3000
+}
+}
+}
+],
+"reviews": 44,
+"stars": 3.3,
+"name": "leather sofa",
+"description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+"company": "caressa"
+}
