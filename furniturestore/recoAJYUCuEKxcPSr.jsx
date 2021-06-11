42fa1d4dce41a055f63f3b04bf3de1a7ab2ec195
khanna1223 <79308199+khanna1223@users.noreply.github.com>
2021-06-11 14:55:05 +1000

    Create recoAJYUCuEKxcPSr.jsx

---
+++ furniturestore/recoAJYUCuEKxcPSr.jsx
@@ -0,0 +1,139 @@
+{
+"id": "recoAJYUCuEKxcPSr",
+"stock": 9,
+"price": 31999,
+"featured": true,
+"colors": [
+"#ffb900",
+"#ff0000",
+"#00ff00"
+],
+"category": "kids",
+"images": [
+{
+"id": "attBZUlve1ndfRX0y",
+"url": "https://dl.airtable.com/.attachments/7e3a7ce2ef8e11da40f1b6617e4885e3/565bf916/product-7.jpg",
+"filename": "product-7.jpg",
+"size": 162415,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/3432e4de70167c4940ef025a0e90ea54/f7a07bf2",
+"width": 64,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/1e4a818f5184993e430420a152315b40/873c7094",
+"width": 910,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/9a2f5f2b316a8903e4239dde28e36514/ef1711f6",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attckGkwgtXrlfpNt",
+"url": "https://dl.airtable.com/.attachments/f15406a7d5734462ccbe033523ab44f2/36f645ba/extra-1.jpeg",
+"filename": "extra-1.jpeg",
+"size": 102108,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/d2795da1eb5123dab799ac0be9210724/bbe8d881",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/85833403499f25d19424c43441381566/ea9a82b8",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/6b639b340be4014d90adbb443b665090/a953b3a9",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attGAW70rinkXZ0ja",
+"url": "https://dl.airtable.com/.attachments/7a2335390d5403fa6f30e5da7c529a1a/8074a08a/extra-2.jpeg",
+"filename": "extra-2.jpeg",
+"size": 84418,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/68ad322534e287275b3ce58b15b352cf/686f1b23",
+"width": 50,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/90daed7d8bf53c66ebb57f1688ddc029/cca4fad1",
+"width": 717,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/31142dd8ad1d7eb482661109270c2f1d/1cd0fbde",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attDlfifELNsb9nAD",
+"url": "https://dl.airtable.com/.attachments/f026e088c27b48e3d0333c1bb2f89875/8323e097/extra-3.jpeg",
+"filename": "extra-3.jpeg",
+"size": 107838,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/400cf72e44540fcd58d3dd326c876562/c2c32e29",
+"width": 55,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/9d54b98347d1b439c0bdabe88a2eb9d4/2d6f8d32",
+"width": 788,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/f4b453807f10e8e2ed3ecd2aadc1bee0/0bfb3785",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attuDVmDOf5dcLKUn",
+"url": "https://dl.airtable.com/.attachments/e789790777f02ec1068c6e12a31b9b8d/9f9968d0/extra-4.jpeg",
+"filename": "extra-4.jpeg",
+"size": 99481,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/3884c0c05fa37f15e94acda42cd53d59/b45a2137",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/595ee95e966d569eb09917ed527851d9/083f857b",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/b8d555089f78e73caf3c220b9d20a772/5223b338",
+"width": 3000,
+"height": 3000
+}
+}
+}
+],
+"reviews": 27,
+"stars": 4.2,
+"name": "modern bookshelf",
+"description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+"company": "caressa"
+}
