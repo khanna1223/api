c8f2f11e445ddd310023bcc17b70e89c656ae3a4
khanna1223 <79308199+khanna1223@users.noreply.github.com>
2021-06-11 15:00:00 +1000

    Create recv2ohxljlK2FZO7.jsx

---
+++ furniturestore/recv2ohxljlK2FZO7.jsx
@@ -0,0 +1,137 @@
+{
+"id": "recv2ohxljlK2FZO7",
+"stock": 49,
+"price": 250099,
+"colors": [
+"#000",
+"#ffb900"
+],
+"category": "bedroom",
+"images": [
+{
+"id": "attqGTNA2noYv5Atq",
+"url": "https://dl.airtable.com/.attachments/856dda1f341b8ec5c6289d2b4212e65b/5a2b639e/0-pexels-burst-545012.jpg",
+"filename": "0-pexels-burst-545012.jpg",
+"size": 790844,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/91bba66b52986f0b1d3e9e6b1e1bf4d0/88caad7b",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/1d692023f254ca11a3d1a3628d198081/e922a771",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/dc1285c00dcc9c8aa80299b9ca59e9b8/9fdd9342",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attDlZ0iE0dCeh8ft",
+"url": "https://dl.airtable.com/.attachments/83abe176c2a619f7826994bf823f3792/391e04b4/z-extra-1.jpeg",
+"filename": "z-extra-1.jpeg",
+"size": 102108,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/c9192101123665698159ef00ed379d3b/b41f208b",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/60f7791ddfbf74f7600c62eac59bb3f7/88687db1",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/ef11f5e1da359d872f1cf24e38225130/f88107dd",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "att6jBeOvNfVR8DJl",
+"url": "https://dl.airtable.com/.attachments/03d6e665a416e80a8b90ce67f1105cbd/527e0481/z-extra-2.jpeg",
+"filename": "z-extra-2.jpeg",
+"size": 84418,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/1084ac13f4294045de99792d24e7bc4e/32df7657",
+"width": 50,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/e681bfe5e2225542ccaad53a4f170f14/0aae8f84",
+"width": 717,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/e882eb5bf0d704c1fd0f4f3a35fdc10f/cb34c3ab",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attznt4OUfTyl3ZKI",
+"url": "https://dl.airtable.com/.attachments/dca1f0bf4c8584d666dd96920744c0e2/d5bdff32/z-extra-3.jpeg",
+"filename": "z-extra-3.jpeg",
+"size": 107838,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/f9ff7245e0b9b9f3b5d3515246927ee6/c2f0c6a0",
+"width": 55,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/2aa83ad3dd8c18cb4d67297f3ef45761/34eab71e",
+"width": 788,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/3aff9a63b6bf8269f027b2fb68836858/93a6d94a",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attAIw08Z4U5Qjfpy",
+"url": "https://dl.airtable.com/.attachments/5115389c97c310f22352351f86242fa3/97befd4a/z-extra-4.jpeg",
+"filename": "z-extra-4.jpeg",
+"size": 99481,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/e5afe84d2a6d78230f9fc239dfcbf67c/e2d976ca",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/5248407eb16e518a2a990aa421ba82a6/a3ebf8c3",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/92c94a08a9181a3c4d4d8ba8a57233a8/43a85b28",
+"width": 3000,
+"height": 3000
+}
+}
+}
+],
+"reviews": 300,
+"stars": 4.2,
+"name": "wooden bed",
+"description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+"company": "ikea"
+}
