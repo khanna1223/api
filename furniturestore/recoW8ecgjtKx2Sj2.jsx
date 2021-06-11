a9c90c53dedf14bbc6cb8bd7a8150c5f4afbcfc6
khanna1223 <79308199+khanna1223@users.noreply.github.com>
2021-06-11 14:53:57 +1000

    Create recoW8ecgjtKx2Sj2.jsx

---
+++ furniturestore/recoW8ecgjtKx2Sj2.jsx
@@ -0,0 +1,138 @@
+{
+"id": "recoW8ecgjtKx2Sj2",
+"stock": 300,
+"price": 20099,
+"colors": [
+"#ff0000",
+"#ffb900",
+"#00ff00"
+],
+"category": "bedroom",
+"images": [
+{
+"id": "attR0yvedJlOyw4R0",
+"url": "https://dl.airtable.com/.attachments/543ddbfca798cbc4c56117a4bfde92e3/212c0527/0-pexels-valeria-boltneva-827518.jpg",
+"filename": "0-pexels-valeria-boltneva-827518.jpg",
+"size": 451186,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/b3a80b798d8c96e729d44428b4394bca/65e84aa5",
+"width": 24,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/d3174ad774fc628e1d50b77e3bec399f/1de7b97a",
+"width": 512,
+"height": 768
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/8528e5a1b8ea6952ccdb8d1e64913541/dabffa28",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attEoENSMB4xQ5zrn",
+"url": "https://dl.airtable.com/.attachments/962b224f18f543f0c4d38e6d9d162792/87a222b8/z-extra-1.jpeg",
+"filename": "z-extra-1.jpeg",
+"size": 102108,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/d1338edbbb96a1527bb7538dcfe7c83c/4f24bd51",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/1e8d1ef68d6c988c85172f057669f591/4ee15cf1",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/e5639a433a0cf3ececc3348512d429f8/7e20f9ba",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "att0wgkeJYYXnFdOo",
+"url": "https://dl.airtable.com/.attachments/4c48b0b8d8e1d1cf96c470bc301333f2/ec529cd3/z-extra-2.jpeg",
+"filename": "z-extra-2.jpeg",
+"size": 84418,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/7083e7724e1cec8ba4de53e427937dbd/21f52e82",
+"width": 50,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/0bffa080635a1f1350f7f0dcad706759/bc01f9a0",
+"width": 717,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/3ec388caf6ec698dc79f6d526f7bbb83/5500072f",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attMwh7hPmzRA0PrN",
+"url": "https://dl.airtable.com/.attachments/37391a38f9b983c25bb5e2dc1e512795/4d9f7cda/z-extra-3.jpeg",
+"filename": "z-extra-3.jpeg",
+"size": 107838,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/13c14f1f4edaa17b31b73f32e0b1f9dc/49b09af4",
+"width": 55,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/c5603b18a7c11c7551731f2188953e68/0f6e5a6c",
+"width": 788,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/1fc6b47c850c438214db39933898df87/2cfce0ae",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attyZVAoqj4AfhJ4S",
+"url": "https://dl.airtable.com/.attachments/0188cbf043409517f87b69ed998afe5e/f64ef065/z-extra-4.jpeg",
+"filename": "z-extra-4.jpeg",
+"size": 99481,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/958485010ac4eb16c908c4c89b0c5819/9ae31b62",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/82771acdadce34dfbce51cc76aa0a6e8/78421cf2",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/fefd6c491b238d2fa3b21e180b43adf3/72978c36",
+"width": 3000,
+"height": 3000
+}
+}
+}
+],
+"reviews": 17,
+"stars": 4.8,
+"name": "leather chair",
+"description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+"company": "caressa"
+}
