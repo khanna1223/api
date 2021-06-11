b2a7ebc4cffd89a54b6e4ad2325c52a585d0ac65
khanna1223 <79308199+khanna1223@users.noreply.github.com>
2021-06-11 14:51:22 +1000

    Create recotY5Nh00DQFdkm.jsx

---
+++ furniturestore/recotY5Nh00DQFdkm.jsx
@@ -0,0 +1,139 @@
+{
+"id": "recotY5Nh00DQFdkm",
+"stock": 5,
+"price": 42999,
+"shipping": true,
+"colors": [
+"#00ff00",
+"#0000ff",
+"#ff0000"
+],
+"category": "dining",
+"images": [
+{
+"id": "attPSCwr1004so4XS",
+"url": "https://dl.airtable.com/.attachments/280143b4696f59a752248eff91479cf2/7d1d63d6/product-8.jpg",
+"filename": "product-8.jpg",
+"size": 281908,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/40b4820899171e0303afa33f33c69198/fe7af9be",
+"width": 25,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/7a38cf782907773d9900165530cfa583/d9f41960",
+"width": 512,
+"height": 743
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/aa3c8412e57e145400bd794204137587/1fed50d5",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attgYeVHxy8rhBglD",
+"url": "https://dl.airtable.com/.attachments/bfe7255a8db7cbbae5d5b3d212295a06/ec5cc3e1/extra-1.jpeg",
+"filename": "extra-1.jpeg",
+"size": 102108,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/08f95bb109cdaca4d817211a56d97cfe/09906b79",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/a66c79271b3f43ebb0488683509674a2/c0acedcc",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/7344a213a8548cc9cd04ff1bfba5c8f3/3578ab11",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attkHermmIOYPnXYE",
+"url": "https://dl.airtable.com/.attachments/c49981330fb1ac7523db2781dac0cb3f/8b862618/extra-2.jpeg",
+"filename": "extra-2.jpeg",
+"size": 84418,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/979e40e83a4daaa88e5ffcb06d22c83b/c6f06c82",
+"width": 50,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/de5f327e2a41ffdc32f79e7ec21f4db0/2d6d8085",
+"width": 717,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/dc2c27d79f2ef1847ae9b5c5d32a7a85/fe121886",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attd3Lck3toObOwUI",
+"url": "https://dl.airtable.com/.attachments/6f8be97ec36c1979fb128a8cd0c425b5/c8e80528/extra-3.jpeg",
+"filename": "extra-3.jpeg",
+"size": 107838,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/cfa292584ee711d8bf70aa71fbc52e9e/ba0bbf7c",
+"width": 55,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/414fb6bbeff6b8dbcb9f4ef5689bd70c/7c589672",
+"width": 788,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/f648f68ff310aaef095a1159ec8bb2f6/2b26c78e",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attU2rh2B6WLCRNmf",
+"url": "https://dl.airtable.com/.attachments/6d13dac549bb3c56933f2ad4eb69a5c5/4b345267/extra-4.jpeg",
+"filename": "extra-4.jpeg",
+"size": 99481,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/ebc2dba4ad087c146f6d8837136bd3dc/f575161c",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/e6b410bd6d47c74a0300bcfbb32f28ee/ab253b07",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/64f6b3fae2001652d3d473e8ee1ad76d/5df4457d",
+"width": 3000,
+"height": 3000
+}
+}
+}
+],
+"reviews": 200,
+"stars": 1.2,
+"name": "dining table",
+"description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+"company": "ikea"
+}
