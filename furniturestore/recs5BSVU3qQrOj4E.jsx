1b645f0f49b83a698f5271fde1a96d3bdba74f6b
khanna1223 <79308199+khanna1223@users.noreply.github.com>
2021-06-11 14:57:36 +1000

    Create recs5BSVU3qQrOj4E.jsx

---
+++ furniturestore/recs5BSVU3qQrOj4E.jsx
@@ -0,0 +1,138 @@
+{
+"id": "recs5BSVU3qQrOj4E",
+"stock": 6,
+"price": 129999,
+"shipping": true,
+"colors": [
+"#00ff00",
+"#ffb900"
+],
+"category": "living room",
+"images": [
+{
+"id": "attJhqAreDsXGSzEr",
+"url": "https://dl.airtable.com/.attachments/4525e10f3ca7e142dd9aa85c6d1db855/51dbd5f0/product-11.jpeg",
+"filename": "product-1 (1).jpeg",
+"size": 56151,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/602e096a38f43d82c0c14db4ea5154d1/228bd69f",
+"width": 58,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/fe9d4f25fee307f6ae5b7a462b70b942/031464c4",
+"width": 819,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/ca5406eae299db99e8623d9bfa0b56f7/95d3d900",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "att7sxKRw79YX17Ki",
+"url": "https://dl.airtable.com/.attachments/c1242b6d80353f8e9fe7b4396135dc46/fc8119a0/extra-1.jpeg",
+"filename": "extra-1.jpeg",
+"size": 102108,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/1cc2b533fa913a3c8fc258cbf86caa0e/9bd40951",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/398caeae7f6a77a5b7a9ca00ed82265c/40996270",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/89fcd86c2806950156df6322ce911981/c6067860",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attMjHB17aDpbmLn9",
+"url": "https://dl.airtable.com/.attachments/5387a6eb4e27a95afd4364db9a6b7987/1d44f2f4/extra-2.jpeg",
+"filename": "extra-2.jpeg",
+"size": 84418,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/cc59860dd4cff7593cc5df03f753742c/849ae43f",
+"width": 50,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/ed97c30e1ee1a13fdcbeafb94f31762e/04e6e3ae",
+"width": 717,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/1610c3683477658e98163d569632b426/0dd34b84",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "att7JHwRt79f2OYmX",
+"url": "https://dl.airtable.com/.attachments/5c0746c57e02ab03fd77da2611b0bd7a/ac4cd33f/extra-3.jpeg",
+"filename": "extra-3.jpeg",
+"size": 107838,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/b22d8144a9ba823988dafcd149d91149/b465662b",
+"width": 55,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/d417977e63a48aacfd73fc8a5153f93e/72519ca5",
+"width": 788,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/55c9d7daf9ee6fb97172be7b2881dd03/a509f368",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attm9198cijWWcI0v",
+"url": "https://dl.airtable.com/.attachments/852df4d4db50f96fb988fdf2c814c81d/be5215f0/extra-4.jpeg",
+"filename": "extra-4.jpeg",
+"size": 99481,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/da212ca609beb8a39ff8d8a859bd5263/92f4e0fa",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/8941a396c8b042ea190c2cec02acc0db/2197ba50",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/099e7784264557a8c3fa692ae4b11bb7/537822fd",
+"width": 3000,
+"height": 3000
+}
+}
+}
+],
+"reviews": 45,
+"stars": 4.8,
+"name": "sofa set",
+"description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+"company": "marcos"
+}
