19a968a6c3f449749d3e26714f482edb0ef13519
khanna1223 <79308199+khanna1223@users.noreply.github.com>
2021-06-11 15:01:08 +1000

    Create recm7wC8TBVdU9oEL.jsx

---
+++ furniturestore/recm7wC8TBVdU9oEL.jsx
@@ -0,0 +1,137 @@
+{
+"id": "recm7wC8TBVdU9oEL",
+"stock": 33,
+"price": 40099,
+"colors": [
+"#0000ff",
+"#00ff00"
+],
+"category": "office",
+"images": [
+{
+"id": "attphTeTAnYbUm6rp",
+"url": "https://dl.airtable.com/.attachments/a92996b7e7a4cb9910fc339b5a014118/a66b3afa/0-product.jpg",
+"filename": "0-product.jpg",
+"size": 124724,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/cc488041707a4a1269a56227c0ce45fa/30628b25",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/954dfa5c8ce3df84a3c7254481464366/a3bd8c4a",
+"width": 766,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/f9ef644a4541fe930744c2b3c20c62ed/676b5bca",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attOsR2TGiQrBkKFS",
+"url": "https://dl.airtable.com/.attachments/8c3762845d4b38db7ebf17b02bbadb5b/cd57d4dd/z-extra-1.jpeg",
+"filename": "z-extra-1.jpeg",
+"size": 102108,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/46790cc7f3c67de10417eb81d4efe212/6b7979ff",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/b8a9a1172c09b644ade0ec69a3c6833b/d82035f9",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/b1d9334b825e457fb6eaa13b73b3e7b6/69fd7980",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attEcDXd9fnEao99y",
+"url": "https://dl.airtable.com/.attachments/74ea6193cf016745cfeb9330514204c6/605b9d83/z-extra-2.jpeg",
+"filename": "z-extra-2.jpeg",
+"size": 84418,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/6f935d791da78ad69bcd9367f1d3a60b/9bb69f10",
+"width": 50,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/05c6f09d9f751d5d6ae8c6440b8871df/d2b346e4",
+"width": 717,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/873094cfe6decb3a90899c8ad3ac92f0/dd07c763",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "att5tSN2fEHkzO4nr",
+"url": "https://dl.airtable.com/.attachments/5c2ccd609f06a36e6f45c4829709e57b/0df280ac/z-extra-3.jpeg",
+"filename": "z-extra-3.jpeg",
+"size": 107838,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/845896552b141263986dfea22ac0893b/8892a66f",
+"width": 55,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/a6d896fc55dbfd0ba40c4df60450cd78/b4b49951",
+"width": 788,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/22640d75975587be6792066f56522a2e/2a916408",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attnAuvy9H4WkEiqj",
+"url": "https://dl.airtable.com/.attachments/61094f141375a8816f23597abe5e811f/3d95a7fb/z-extra-4.jpeg",
+"filename": "z-extra-4.jpeg",
+"size": 99481,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/3401d89411f9d33d2ead652ad2e312be/d34dc2c6",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/a76fb4ef6b98865d835d3d28ea554466/17693f74",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/4136e72803af3532dde831ea3405660d/cb37636b",
+"width": 3000,
+"height": 3000
+}
+}
+}
+],
+"reviews": 14,
+"stars": 3.2,
+"name": "wooden desk",
+"description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+"company": "ikea"
+}
