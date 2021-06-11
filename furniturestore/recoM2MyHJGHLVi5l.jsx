d0821d7a59e5e9d4487e6fab548c4c61d8e8813f
khanna1223 <79308199+khanna1223@users.noreply.github.com>
2021-06-11 14:50:34 +1000

    Create recoM2MyHJGHLVi5l.jsx

---
+++ furniturestore/recoM2MyHJGHLVi5l.jsx
@@ -0,0 +1,137 @@
+{
+"id": "recoM2MyHJGHLVi5l",
+"stock": 20,
+"price": 4099,
+"shipping": true,
+"colors": [
+"#000"
+],
+"category": "dining",
+"images": [
+{
+"id": "attjNsGwwTxMqthdk",
+"url": "https://dl.airtable.com/.attachments/7442c132471b205b0883929efd5a96af/16a65ea6/z-0-product.jpg",
+"filename": "z-0-product.jpg",
+"size": 1009025,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/e7631376d6bdcbac41b0402e38126eb9/35311e1c",
+"width": 36,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/a6119fabf7256049cc0e8dbcdf536c9c/b0153f66",
+"width": 519,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/b7f11ac2805365499a48fb5cb607a128/7ae4251b",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attSwKNXmgpwfNxmt",
+"url": "https://dl.airtable.com/.attachments/4dad605e632ec2971f2235a996329952/63ae6f9a/z-extra-1.jpeg",
+"filename": "z-extra-1.jpeg",
+"size": 102108,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/19a316ce4a373ea6073ea01b10a6b460/a55db7f3",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/3ce1ad9566bbfdb6ff82eb38b626a83b/2eb62001",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/ee86bc50348aeb2b1b02b2479eb26601/99446692",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "atthDyZxcNhcsU368",
+"url": "https://dl.airtable.com/.attachments/4ab93148649c1559050398743f713710/1c3f6916/z-extra-2.jpeg",
+"filename": "z-extra-2.jpeg",
+"size": 84418,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/ab9602f579b03e593ca2b646a0d0e395/55fab158",
+"width": 50,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/f06cb6cbc42ed5350c0dfa94f31b0d98/6d47ccba",
+"width": 717,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/e31045debc59f892bad9ea03d6266f63/a8dd4e11",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attobC2UOlSB5o8qL",
+"url": "https://dl.airtable.com/.attachments/6370c196d2b2f7e26c23daf4a0205b9b/7b5cfeed/z-extra-3.jpeg",
+"filename": "z-extra-3.jpeg",
+"size": 107838,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/88cc2660de3cf6238fb25afa9dca560b/64a39730",
+"width": 55,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/9052207c5f09d70d0271084e6dbcc218/22562561",
+"width": 788,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/b5c733e961299caa4550b091e48410bd/d8714093",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attehkCUpn2JKybDe",
+"url": "https://dl.airtable.com/.attachments/88d5851bd98cade822c466eab7d50bd4/f11d5ee8/z-extra-4.jpeg",
+"filename": "z-extra-4.jpeg",
+"size": 99481,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/4ea43729de81908ab32d389104a243d6/34719eec",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/ebeae57cca7a2d07dd247da16dad3595/d29e50c5",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/0b14f85ae3c4d146480d8338355547a6/bc40930d",
+"width": 3000,
+"height": 3000
+}
+}
+}
+],
+"reviews": 11,
+"stars": 4.2,
+"name": "bar stool",
+"description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+"company": "liddy"
+}
