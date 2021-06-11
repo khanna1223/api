6c446326862d76b7f761f4d87236572d5fb76807
khanna1223 <79308199+khanna1223@users.noreply.github.com>
2021-06-11 14:55:43 +1000

    Create recQ0fMd8T0Vk211E.jsx

---
+++ furniturestore/recQ0fMd8T0Vk211E.jsx
@@ -0,0 +1,137 @@
+{
+"id": "recQ0fMd8T0Vk211E",
+"stock": 10,
+"price": 3099,
+"shipping": true,
+"colors": [
+"#000"
+],
+"category": "living room",
+"images": [
+{
+"id": "attDr2GEfkjH9jSuC",
+"url": "https://dl.airtable.com/.attachments/e2eef862d9b7a2fb0aa74fa24fbf97bb/25c4bc17/0-pexels-pixabay-462235.jpg",
+"filename": "0-pexels-pixabay-462235.jpg",
+"size": 68729,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/5262150bf763c5679db0e3cee42be24e/8d83a281",
+"width": 24,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/89ba7458c24252be77f5a835dd398880/c13ef359",
+"width": 512,
+"height": 768
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/0f0a5c297eb016cfe26d4fd76c20339d/e4f74181",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "att20IBQhatV1xDaV",
+"url": "https://dl.airtable.com/.attachments/f4720cc51a45ccc204f7476d51cb1b0e/eeb5fe4e/z-extra-1.jpeg",
+"filename": "z-extra-1.jpeg",
+"size": 102108,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/4e68eb6b0e70ef5982d9fb28b789620b/ecb92dc6",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/dfebe4693c2ad9260848b3b10e06af2c/4d93a65d",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/c75787cb341d578a270a524566d9bc44/c009653d",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attqAL3HG0DuEtzIM",
+"url": "https://dl.airtable.com/.attachments/a73777f8a2cbf4820ccaa6aa4349db01/c541de4b/z-extra-2.jpeg",
+"filename": "z-extra-2.jpeg",
+"size": 84418,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/40600e92a2841a0744690541c6718848/c58a800f",
+"width": 50,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/94ad782cafa15276fb55b4cd355164c2/bd284ad7",
+"width": 717,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/7d9139d3e81a689dc1f6eec9a4036a11/7d190786",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attIQHlf8u7jXZtLV",
+"url": "https://dl.airtable.com/.attachments/7a50daf83875879b373d91ebb9bb6012/c1695f7e/z-extra-3.jpeg",
+"filename": "z-extra-3.jpeg",
+"size": 107838,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/d5ef0ccf4a5db4b114ce5abcfc27ca3c/c2c6dd81",
+"width": 55,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/6fc3a3082ab28f2262148f90604b8414/9bc675b2",
+"width": 788,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/01093553b0f6958662246ceef7295aa2/9818688c",
+"width": 3000,
+"height": 3000
+}
+}
+},
+{
+"id": "attJzGj3FZyfXxIxq",
+"url": "https://dl.airtable.com/.attachments/5592998dcaee77b12c50bda63dd94d06/6ad61540/z-extra-4.jpeg",
+"filename": "z-extra-4.jpeg",
+"size": 99481,
+"type": "image/jpeg",
+"thumbnails": {
+"small": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/415eac3ff1061b860c05135789f49540/3ac25304",
+"width": 54,
+"height": 36
+},
+"large": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/d2f77985e273e799783c80222751ecb4/3c12efcd",
+"width": 768,
+"height": 512
+},
+"full": {
+"url": "https://dl.airtable.com/.attachmentThumbnails/fc3e8cd299a4bd28943f4508db736221/26d8c5d4",
+"width": 3000,
+"height": 3000
+}
+}
+}
+],
+"reviews": 100,
+"stars": 2.2,
+"name": "modern poster",
+"description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+"company": "liddy"
+}
